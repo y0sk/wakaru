38109ee573c091ee305dfb7e62eeb7bdba808cfb
y0sk <ysk06284968128@gmail.com>
2020-07-26 17:20:49 +0900

    migration 修正

--- db/migrate/20200717104028_add_columns_on_check_test.rb
+++ db/migrate/20200717104028_add_columns_on_check_test.rb
@@ -1,6 +1,6 @@
 class AddColumnsOnCheckTest < ActiveRecord::Migration[5.0]
   def change
-    #add_reference :check_tests, :material, foreign_key: true
+    add_reference :check_tests, :material, foreign_key: true
     add_column :check_tests, :starts_from, :integer
     add_column :check_tests, :ends_to, :integer
     add_column :check_tests, :chapter, :string
--- db/migrate/20200718100525_drop_image_of_check_test_sentece.rb
+++ db/migrate/20200718100525_drop_image_of_check_test_sentece.rb
@@ -1,5 +1,5 @@
 class DropImageOfCheckTestSentece < ActiveRecord::Migration[5.0]
   def change
-    drop_table :images_of_check_test_sentences
+    #drop_table :images_of_check_test_sentences
   end
 end
