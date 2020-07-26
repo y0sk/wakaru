ec3d6dbf0dccdb8efebf62a6807d91f525f6bb51
y0sk <ysk06284968128@gmail.com>
2020-07-26 17:11:00 +0900

    migration 修正

--- db/migrate/20200711210736_remove_student_id_on_check_test.rb
+++ db/migrate/20200711210736_remove_student_id_on_check_test.rb
@@ -1,5 +1,5 @@
 class RemoveStudentIdOnCheckTest < ActiveRecord::Migration[5.0]
   def change
-    remove_references :check_tests, :student_id, foreign_key: true
+    remove_reference :check_tests, :student, foreign_key: true
   end
 end
