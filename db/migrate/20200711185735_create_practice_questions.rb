3d37c08bb353774ed82c544901d0d47824e6a6c8
y0sk <ysk06284968128@gmail.com>
2020-07-26 17:15:53 +0900

    migration 修正

--- db/migrate/20200711185735_create_practice_questions.rb
+++ db/migrate/20200711185735_create_practice_questions.rb
@@ -3,7 +3,7 @@ class CreatePracticeQuestions < ActiveRecord::Migration[5.0]
     create_table :practice_questions do |t|
        t.timestamps :started_time
         t.timestamps :ended_time
-        #t.references :check_test_element, foreign_key: true
+        t.references :check_test_element, foreign_key: true
 
     end
   end
--- db/migrate/20200712102643_remove_source_picture_on_practice_question.rb
+++ db/migrate/20200712102643_remove_source_picture_on_practice_question.rb
@@ -1,5 +1,5 @@
 class RemoveSourcePictureOnPracticeQuestion < ActiveRecord::Migration[5.0]
   def change
-    remove_column :practice_questions, :source_picture
+    #remove_column :practice_questions, :source_picture
   end
 end
