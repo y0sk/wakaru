37d5c12586cc3b73926d67970798a6fc2179777f
y0sk <ysk06284968128@gmail.com>
2020-07-26 17:25:22 +0900

    migration 修正

--- db/migrate/20200719160654_add_references_on_practice_question.rb
+++ db/migrate/20200719160654_add_references_on_practice_question.rb
@@ -1,5 +1,5 @@
 class AddReferencesOnPracticeQuestion < ActiveRecord::Migration[5.0]
   def change
-    add_reference :practice_questions, :check_test_sentence, foreign_key: true
+    #add_reference :practice_questions, :check_test_sentence, foreign_key: true
   end
 end
