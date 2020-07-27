0a0324d2c833191a84080e1ee15ec1fe9fe39310
y0sk <ysk06284968128@gmail.com>
2020-07-27 23:53:29 +0900

    mysql dump

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -1,18 +1,4 @@
 ActiveAdmin.register Student do
+  permit_params :email, :name, :password_digest
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :email, :password_digest
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :email, :password_digest]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
 end
